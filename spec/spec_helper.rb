4f82df7745802d546aaf14de311dd0cffb244187
Robert Fletcher <lobatifricha@gmail.com>
2022-06-19 09:35:45 -0700

    Prevent system calls in tests
    This replaces `` Kernel#` `` and `Kernel#system` to prevent using them
    in `Baes` code. Instead we should prefer using `Open3.capture3` for
    consistency and so that we can easily stub them out.
    
    Also stub out `Open3.capture3` to prevent accidentally making system
    calls in tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,22 @@ end
 
 require "baes"
 
+class TestingError < StandardError; end
+
+module Kernel
+  def `(command)
+    raise TestingError,
+          "Don't use system calls. " \
+          "Use `Open3.capture3` instead. Called with `#{command}`"
+  end
+
+  def system(command)
+    raise TestingError,
+          "Don't use system calls. " \
+          "Use `Open3.capture3` instead. Called with `#{command}`"
+  end
+end
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
@@ -17,5 +33,9 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  config.before do
+    allow(Open3).to receive(:capture3)
   end
 end
